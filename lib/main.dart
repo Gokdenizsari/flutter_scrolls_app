4f6d205da7fe22e48da68787161e0f30f8521aa7
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-07-27 17:07:33 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_scrolls_app/card_and_listtile.dart';
+import 'package:flutter_scrolls_app/listview_kullan%C4%B1m%C4%B1.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Material App', home: CardAndListtile());
+    return MaterialApp(title: 'Material App',
+     home: ListViewKullanimi());
   }
 }
