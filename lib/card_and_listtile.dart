1482242003dd5a4186f80b2230699133c9298fc2
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-07-27 16:20:12 +0300

    Scrolls

---
+++ lib/card_and_listtile.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CardAndListtile extends StatelessWidget {
+  const CardAndListtile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CardAndListtile"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            tekListe(),
+            tekListe(),
+            tekListe(),
+            tekListe(),
+            tekListe(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Column tekListe() {
+    return Column(
+      children: [
+        Card(
+          color: Colors.teal.shade200,
+          shadowColor: Colors.purple.shade200,
+          elevation: 6,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          child: ListTile(
+            leading: CircleAvatar(
+                child: Icon(
+              Icons.add,
+              color: Colors.black,
+            )),
+            title: Text("Üst Bilgi"),
+            subtitle: Text("Alt Bilgi"),
+            trailing: Icon(Icons.add_circle_outlined),
+          ),
+        ),
+        Divider(
+          color: Colors.orange.shade300,
+          thickness: 1,
+          height: 20,
+          endIndent: 30,
+          indent: 30,
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_scrolls_app/card_and_listtile.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Material App', home: CardAndListtile());
+  }
+}
